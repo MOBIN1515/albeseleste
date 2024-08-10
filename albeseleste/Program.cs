27e7474bffbfa77fd426179f04a008844f13d0d1
Mobin <Mobin@DESKTOP-NIS2M55>
2024-08-10 17:19:04 +0330

    Add project files.

---
+++ albeseleste/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace albeseleste
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("please enter number1 ");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("please enter number2 ");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            int sum = num1 + num2;
+            Console.WriteLine("number1+number2 : " +  + sum);
+
+
+
+
+
+
+
+
+        }
+    }
+}
